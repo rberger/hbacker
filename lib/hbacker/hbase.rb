d0e79b43ca6d10a9d673ead13bff0e5b2d3e7872
Robert J. Berger <rberger@ibd.com>
2011-03-18 02:08:46 -0700

    Test and implementation for Hbacker::Hbase initializer

--- lib/hbacker/hbase.rb
+++ lib/hbacker/hbase.rb
@@ -1,17 +1,20 @@
 module Hbacker
   require "stargate"
   
-  class HBase
+  class Hbase
     attr_reader :hbase_home, :hadoop_home, :star_gate
     
-    def initialize(master_host, master_port, hbase_home, hadoop_home)
+    def initialize(hbase_home, hadoop_home, master_host, master_port=nil)
       @hbase_home = hbase_home
       @hadoop_home = hadoop_home
-      @stargate = Stargate::Client.new("http://master_host:master_port")
+      @master_host = master_host
+      @master_port = master_port
+      @master_port_string = master_port.nil? ? "" : ":#{master_port}" 
+      puts "hbase_home: #{hbase_home.inspect} hadoop_home: #{hadoop_home.inspect} master_host: #{master_host.inspect} master_port: #{master_port.inspect}"
+      puts "http://#{@master_host}#{@master_port_string}"
+      @stargate = Stargate::Client.new("http://#{@master_host}#{@master_port_string}")
+      puts "@stargate: #{@stargate.inspect}"
     end
-    
-    def 
-    def 
   end
 end
 
---
+++ spec/hbacker/hbase_spec.rb
@@ -0,0 +1,23 @@
+require File.join(File.dirname(__FILE__), "..", "spec_helper")
+
+require "hbacker"
+require "hbacker/hbase"
+require "stargate"
+require "fog"
+
+describe Hbacker::Hbase, "initialize" do
+  before :each do
+    @host = 'hbase-master0-production.runa.com'
+    @port = 8808
+    @hbase_hm = "/mnt/hbase"
+    @hadoop_hm = "/mnt/hadoop"
+    @stargate = mock('@stargate')
+    Stargate::Client.new.stub(:new).and_return(@stargate)
+  
+  end
+  
+  it "should create an instance of Stargate::Client from the correct url with no port specified" do
+    Stargate::Client.should_receive(:new).with('http://hbase-master0-production.runa.com').and_return(@stargate)
+    Hbacker::Hbase.new(@hbase_hm, @hadoop_hm, @host)
+  end
+end
