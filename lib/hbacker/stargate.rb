3cc02e5edc90e1f2ecce985566eb360d3f7ea57f
Robert J. Berger <rberger@ibd.com>
2011-03-17 22:40:07 -0700

    Complete tests on stargate additions

--- lib/hbacker/stargate.rb
+++ lib/hbacker/stargate.rb
@@ -1,14 +1,13 @@
 require 'stargate'
 
 module Stargate
-  module Operation
-    module TableOperation
-      
+  module Model
+    class TableDescriptor < Record
       ##
       # Convert Stargate::Model::TableDescriptor#column_families into an array of hashes
       #
-      def column_families_to_hash(column_families)
-        column_families.collect do |c| 
+      def column_families_to_hash
+        self.column_families.collect do |c| 
           hsh = {}
           c.instance_variables.collect do |i| 
             k = i[1..-1].to_sym
@@ -18,16 +17,22 @@ module Stargate
           hsh
         end
       end
+    end
+  end
+  
+  module Operation
+    module TableOperation
 
       ##
       # Create an HBase table based on the Table Schema represented 
       # by a Stargate::Model::TableDescriptor instance
       # Will use TableDescriptor#name for the new table name unless 
-      # the optional name argument is supplied
+      # the optional table_name argument is supplied
       
-      def create_table_from_table_descriptor(table_descriptor, name=nil)
-        table_name = table_descriptor.name unless name
-        column_family_hashes = column_families_to_hash table_descriptor.column_families
+      def create_table_from_table_descriptor(table_descriptor, table_name=nil)
+        table_name = table_descriptor.name unless table_name
+        
+        column_family_hashes = table_descriptor.column_families_to_hash
         
         self.create_table(table_name, *column_family_hashes)
       end
--- spec/hbacker/hbase-stargate/table_operation_spec.rb
+++ spec/hbacker/hbase-stargate/table_operation_spec.rb
@@ -2,6 +2,46 @@ require File.join(File.dirname(__FILE__), "..", "..", "spec_helper")
 require 'hbacker/stargate'
 require 'stargate'
 
+describe Stargate::Model, "#column_families_to_hash" do
+  before :all do
+    @column_families = []
+    @column_descriptor_0 = Stargate::Model::ColumnDescriptor.new(:name => 'hobbit',
+      :max_version => 100000,
+      :compression => Stargate::Model::CompressionType::NONE,
+      :in_memory => true,
+      :block_cache => false,
+      :ttl => -1,
+      :max_cell_size => 2147483647,
+      :bloomfilter => false)
+
+    @column_families <<  @column_descriptor_0
+
+    @column_descriptor_1 = Stargate::Model::ColumnDescriptor.new(:name => 'sauron',
+      :max_version => 3,
+      :compression => Stargate::Model::CompressionType::NONE,
+      :in_memory => false,
+      :block_cache => true,
+      :ttl => -1,
+      :max_cell_size => 2147483647,
+      :bloomfilter => false)
+      
+    @column_families <<  @column_descriptor_1
+    
+    @table_descriptor = Stargate::Model::TableDescriptor.new(
+      :name => "test", 
+      :column_families => @column_families)
+  end
+    
+  it "should have two column_families" do
+    @table_descriptor.column_families_to_hash.count.should == 2
+  end
+  
+  it "should be accessible as an array of hashes" do
+    @table_descriptor.column_families_to_hash.first[:name].should == 'hobbit'
+    @table_descriptor.column_families_to_hash[1][:name].should == 'sauron'
+  end
+end
+
 describe Stargate::Operation::TableOperation, "#create_table_from_table_descriptor" do
   before :each do
     @connection = mock('@connection')
@@ -9,21 +49,36 @@ describe Stargate::Operation::TableOperation, "#create_table_from_table_descript
     url = ENV["STARGATE_URL"].nil? ? "http://localhost:8080" : ENV["STARGATE_URL"]
     @client = Stargate::Client.new(url)
     @client.stub(:create_table)
-    
-    @column_descriptor_1 = Stargate::Model::ColumnDescriptor.new(:name => 'habbit',
-                                   :max_version => 3,
-                                   :compression => Stargate::Model::CompressionType::NONE,
-                                   :in_memory => false,
-                                   :block_cache => false,
-                                   :ttl => -1,
-                                   :max_cell_size => 2147483647,
-                                   :bloomfilter => false)
+
+    @column_descriptor_1 = Stargate::Model::ColumnDescriptor.new(:name => 'hobbit',
+    :max_version => 3,
+    :compression => Stargate::Model::CompressionType::NONE,
+    :in_memory => false,
+    :block_cache => false,
+    :ttl => -1,
+    :max_cell_size => 2147483647,
+    :bloomfilter => false)
     @column_families = []
     @column_families <<  @column_descriptor_1
+
+    @table_descriptor = Stargate::Model::TableDescriptor.new(:name => "test", :column_families => @column_families)
   end
 
   it "should accept a single argument of type Stargate::Model::TableDescriptor" do
-    table_descriptor = Stargate::Model::TableDescriptor.new(:name => "test", :column_families => @column_families)
-    result = @client.create_table_from_table_descriptor(table_descriptor)
+    result = @client.create_table_from_table_descriptor(@table_descriptor)
+  end
+  
+  it "should accept two arguments of type Stargate::Model::TableDescriptor and string" do
+    result = @client.create_table_from_table_descriptor(@table_descriptor, 'tolken')
+  end
+  
+  it "should generate an error with invalid arguments" do
+    expect { @client.create_table_from_table_descriptor('tolken') }.to raise_error
+  end
+  
+  it "should pass a name and hash to Stargate::Operation::TableOperation#create_table" do
+    @client.should_receive(:create_table).with(instance_of(String), hash_including(:name => 'hobbit'))
+    result = @client.create_table_from_table_descriptor(@table_descriptor, 'tolken')
+    
   end
 end
