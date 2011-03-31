7ec08595c599725af7bb0b5f8ad617ac92e4afbe
Robert J. Berger <rberger@ibd.com>
2011-03-30 23:42:05 -0700

    Snapshot

--- lib/hbacker/helpers.rb
+++ lib/hbacker/helpers.rb
@@ -2,6 +2,14 @@ module Hbacker
   require "hbacker"
   require "stalker"
 
+  ##
+  # Will wait until the Stalker/Beanstalk queue of waiting jobs goes below threshold
+  # @param [Integer] threshold Function will wait until the number of jobs in the queue goes below this value
+  # @param [Integer] timeout Function will return after timeout seconds with a :timeout status
+  # @return [Hash] return_value
+  # @option return_value [Boolean] :timeout Only set if there was a timeout
+  # @option return_value [Boolean] :ok Only set if number of jobs went below the threshold before timeout
+  # @option return_value [Hash] :stats The 
   def wait_for_hbacker_queue(threshold, timeout)
     
   end
